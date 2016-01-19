2ed853301e8cedb72c7c07367d58e55cac23aa7c
winlin <winlin@vip.126.com>
2016-01-19 23:14:36 +0800

    add PI to arduino

---
+++ raspi-pl2303.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import serial
+
+print 'RaspberryPi PL2303(USB2SerialTTL) communicate.'
+print "Copyright (c) 2015 winlin(winlin@vip.126.com)"
+
+f = serial.Serial('/dev/ttyUSB0', 115200)
+
+while True:
+    v = "Hello, Arduino, this is RaspberryPi 2.0~"
+    f.write(v)
+    print 'PI: %s'%(v)
+
+    r = ''
+    for i in v:
+        r += f.read()
+    print 'Arduino: %s'%(r)
