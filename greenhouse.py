479f7eac4d27cbb90fbdb883ee82b24fd73b9a10
winlin <winlin@vip.126.com>
2016-01-21 23:49:37 +0800

    refine greenhource.

--- greenhouse.py
+++ greenhouse.py
@@ -3,61 +3,6 @@
 ########################################################################################
 ########################################################################################
 ########################################################################################
-# depends on rpi.gpio, which can be install by:
-#      sudo apt-get install python-rpi.gpio
-import RPi.GPIO as GPIO
-import time, sys
-
-def bits2byte(x):
-    v = 0
-    for i in range(8):
-        v += x[i] << (7-i)
-    return v
-
-def dht11_sample(channel):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(channel, GPIO.OUT)
-    GPIO.output(channel, GPIO.LOW)
-    time.sleep(0.02)
-    GPIO.output(channel, GPIO.HIGH)
-    GPIO.setup(channel, GPIO.IN)
-
-    while GPIO.input(channel) == GPIO.LOW:
-        continue
-    while GPIO.input(channel) == GPIO.HIGH:
-        continue
-
-    data = []
-    for i in range(40):
-        while GPIO.input(channel) == GPIO.LOW:
-            continue
-        for j in range(100):
-            if GPIO.input(channel) != GPIO.HIGH:
-                break
-        if j < 8:
-            data.append(0)
-        else:
-            data.append(1)
-
-    return data
-
-def dht11_read():
-    try:
-        data=dht11_sample(18) # GPIO18
-    finally:
-        GPIO.cleanup()
-
-    humidity = bits2byte(data[0:8])
-    humidity_point = bits2byte(data[8:16])
-    temperature = bits2byte(data[16:24])
-    temperature_point = bits2byte(data[24:32])
-    check = bits2byte(data[32:40])
-    expect = humidity + humidity_point + temperature + temperature_point
-    return (humidity, humidity_point, temperature, temperature_point, check, expect)
-
-########################################################################################
-########################################################################################
-########################################################################################
 import serial
 
 def pl2303_open(device="/dev/ttyUSB0"):
@@ -66,17 +11,52 @@ def pl2303_open(device="/dev/ttyUSB0"):
 ########################################################################################
 ########################################################################################
 ########################################################################################
+import os, time, datetime, json
+
+def trace(msg):
+    print "%s: %s"%(datetime.datetime.now(), msg)
+
 def loop():
+    with os.popen("python raspi-dht11.py 2>/dev/stdout 1>/dev/null") as f:
+        ret = f.read().strip()
+    if len(ret) == 0:
+        trace("ignore failed to fetch temperature and humidity.")
+        time.sleep(3)
+        return
+
+    obj = json.loads(ret)
+    temperature = obj["t"]
+    humidity = obj["h"]
+    trace("ok, temperature: %s *C, humidity: %s %%"%(temperature, humidity))
+
+    target_temperature = 19
+    if temperature < target_temperature:
+        trace("oh, it's too cold, let's open the heater to keep %s *C"%(target_temperature));
+        arduino.write('H')
     return
+
+def read_artuino(arduino):
+    try:
+        for i in range(5):
+            c = arduino.read(1)
+            if c == 'H':
+                trace("arduino open the heater: %s(%s)"%(c, len(c)))
+            if c == 'C':
+                trace("arduino close the heater %s(%s)"%(c, len(c)))
+    except Exception:
+        pass
+
 if __name__ == "__main__":
     print "Greenhouse use RaspberryPi and Arduino\n" \
           "Copyright (c) 2015 winlin(winlin@vip.126.com)"
+
+    arduino = pl2303_open()
     while True:
         try:
-            print "start greenhouse loop."
+            trace("let's look into the greenhouse.")
             loop()
-            time.sleep(1)
         except Exception,ex:
-            print "ignore except: %s"%ex
-            time.sleep(3)
+            trace("ignore except: %s"%ex)
+            time.sleep(5)
+        read_artuino(arduino)
 
--- raspi-dht11.py
+++ raspi-dht11.py
@@ -3,7 +3,7 @@
 # depends on rpi.gpio, which can be install by:
 #      sudo apt-get install python-rpi.gpio
 import RPi.GPIO as GPIO
-import time, sys
+import time, sys, json
 
 def bits2byte(x):
     v = 0
@@ -25,10 +25,10 @@ def dht11_sample(channel):
         continue
 
     data = []
-    for i in range(40):
+    for i in range(0, 40, 1):
         while GPIO.input(channel) == GPIO.LOW:
             continue
-        for j in range(100):
+        for j in range(0, 100, 1):
             if GPIO.input(channel) != GPIO.HIGH:
                 break
         if j < 8:
@@ -60,6 +60,7 @@ if __name__ == "__main__":
     #print "got data: %s"%(data)
     if check == expect:
         print "ok, temperature: %s *C, humidity: %s %%"%(temperature, humidity)
+        sys.stderr.write(json.dumps({"t":temperature, "h":humidity}))
         sys.exit(0)
     else:
         print "fail, temperature: %s *C, humidity: %s %%, check: %s != %s"%(temperature, humidity, check, expect)
