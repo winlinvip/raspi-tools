75fa22958ccee4e60177c129a10fe66b23ef68be
winlin <winlin@vip.126.com>
2016-01-23 16:00:15 +0800

    remove the python dht11 controller.

--- raspi-dht11.py
+++ raspi-dht11.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 
-import raspi_core, time, sys
-
-if __name__ == "__main__":
-    print "RaspberryPi DH11(temperature+humidity) application\n" \
-        "Copyright (c) 2015 winlin(winlin@vip.126.com)"
-    print "sample at GPIO18 for PI2"
-    try:
-        raspi_core.dht11_init()
-        while True:
-            data=raspi_core.dht11_sample(18) # GPIO18
-            (humidity, humidity_point, temperature, temperature_point, check, expect) = raspi_core.dht11_parse(data)
-            if check == expect:
-                print "ok, temperature: %s *C, humidity: %s %%"%(temperature, humidity)
-            else:
-                print "fail, temperature: %s *C, humidity: %s %%, check: %s != %s"%(temperature, humidity, check, expect)
-            time.sleep(2)
-    finally:
-        raspi_core.dht11_cleanup()
+# Please use Arduino to control the DHT11,
+# for the RaspberryPi Python cannot used to control the us level.
+# https://github.com/winlinvip/simple-dht
+import sys
+sys.exit(1)
